29648df9ef3606db7c9ba4dd3d712e17c20f30d6
殷玉秋 <yinyuqiu@yinyuqiudeMacBook-Air.local>
2017-05-08 20:38:10 +0800

    version 1.0.0

---
+++ FFFJsonKillNullDemo/FFFJsonKillNullDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFFJsonKillNullDemo
+//
+//  Created by 殷玉秋 on 2017/5/8.
+//  Copyright © 2017年 fff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFFJsonKillNullDemo/FFFJsonKillNullDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFFJsonKillNullDemo
+//
+//  Created by 殷玉秋 on 2017/5/8.
+//  Copyright © 2017年 fff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
